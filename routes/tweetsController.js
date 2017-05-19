00d3e74e4f0a03a2a7b356dfdf7c427b6fdc5c06
Jorge Abad <jorgejoseabad@gmail.com>
2017-05-19 09:53:30 +0200

    firs part complete, tweets are saved OK

--- routes/tweetsController.js
+++ routes/tweetsController.js
@@ -1,12 +1,77 @@
 /*jshint esversion:6 */
 const express          = require("express");
 const tweetsController = express.Router();
+// tweetsController.js
+// Models
+const User  = require("../models/user");
+const Tweet = require("../models/tweet");
 
-tweetsController.get("/", (req, res, next) => {
+const moment = require("moment");
+
+//protecting the route, to ensure that all actions must have an authenticated user
+tweetsController.use((req, res, next) => {
+  if (req.session.currentUser) { next(); }
+  else { res.redirect("/login"); }
+});
+
+//route reemplazada por la get siguiente
+/*tweetsController.get("/", (req, res, next) => {
   res.render(
     "tweets/index",
     { username: req.session.currentUser.username}
   );
+});*/
+
+tweetsController.get("/", (req, res, next) => {
+  User
+    .findOne({ username: req.session.currentUser.username }, "_id username")
+    .exec((err, user) => {
+      if (!user) { return; }
+
+      Tweet.find({ "user_name": user.username }, "tweet created_at")
+        .sort({ created_at: -1 })
+        .exec((err, tweets) => {
+          console.log("tweets");
+          res.render("tweets/index",
+            {
+              username: user.username,
+              tweets,
+              moment });
+        });
+  });
 });
+
+
+tweetsController.get("/new", (req, res, next) => {
+  res.render("tweets/new",
+    { username: req.session.currentUser.username });
+});
+
+tweetsController.post("/", (req, res, next) => {
+  const user  = req.session.currentUser;
+
+  User.findOne({ username: user.username }).exec((err, user) => {
+    if (err) { return; }
+
+    const newTweet = Tweet({
+      user_id:   user._id,
+      user_name: user.username,
+      tweet:     req.body.tweetText
+    });
+
+    newTweet.save((err) => {
+      if (err) {
+        res.render("tweets/new",
+          {
+            username: user.username,
+            errorMessage: err.errors.tweet.message
+          });
+      } else {
+        res.redirect("/tweets");
+      }
+    });
+  });
+});
+
 
 module.exports = tweetsController;
